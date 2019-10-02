cb589b147918ae4026b2b371cd3fe0e2769a8ed0
Philipp Hellmich <runningman84@users.noreply.github.com>
2019-10-02 21:18:49 +0200

    Create power.py

---
+++ apps/power.py
@@ -0,0 +1,131 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+#
+# HeatSaver App
+#
+# Args:
+#
+# Concept:
+# PowerOn if motion
+# PowerOff if no motion for motion_duration
+# PowerOff if vacation for vacation_duration
+# PowerOff if nobody at home for tracker_duration unless guest
+# Nothing on startup
+
+class PowerSaver(hass.Hass):
+
+    def initialize(self):
+        self.log("Hello from PowerSaver")
+
+        # setup sane defaults
+        self._motion_sensors = self.args.get("motion_sensors", [])
+        self._device_trackers = self.args.get("device_trackers", [])
+        self._power_controls = self.args.get("power_controls", [])
+        self._vacation_control = self.args.get("vacation_control", None)
+        self._guest_control = self.args.get("guest_control", None)
+        self._motion_duration = self.args.get("motion_duration", 60*60*2)
+        self._tracker_duration = self.args.get("tracker_duration", 60*15)
+        self._vacation_duration = self.args.get("vacation_duration", 60)
+
+        # log current config
+        self.log("Got motion sensors {}".format(
+            self._motion_sensors))
+        self.log("Got device trackers {}".format(self._device_trackers))
+        self.log("Got device controls {}".format(self._power_controls))
+        self.log("Got motion duration {}".format(self._motion_duration))
+        self.log("Got tracker duration {}".format(self._tracker_duration))
+        self.log("Got vacation duration {}".format(self._vacation_duration))
+        self.log("Got {} device_trackers home and {} device_trackers not home".format(
+            self.count_home_device_trackers(), self.count_not_home_device_trackers()))
+        self.log("Got guest_mode {}".format(self.in_guest_mode()))
+        self.log("Got vacation_mode {}".format(self.in_vacation_mode()))
+
+        # start or stop power based on motion
+        for sensor in self._motion_sensors:
+            self.listen_state(self.power_on_callback, sensor,
+                              new="on", old="off")
+            self.listen_state(self.power_off_motion_callback,
+                              sensor, new="off", old="on", duration=self._motion_duration)
+
+        # stop power if nobody is home
+        for sensor in self._device_trackers:
+            self.listen_state(self.power_off_presence_callback,
+                              sensor, new="not_home", old="home", duration=self._tracker_duration)
+
+        # stop power during vacation
+        if self._vacation_control is not None:
+            self.listen_state(self.power_off_presence_callback, self._vacation_control,
+                              new="on", old="off", duration=self._vacation_duration)
+
+    def count_motion(self, state):
+        count = 0
+        for sensor in self._motion_sensors:
+            if self.get_state(sensor) == state:
+                count = count + 1
+        return count
+
+    def count_on_motion(self):
+        return self.count_motion("on")
+
+    def count_off_motion(self):
+        return self.count_motion("off")
+
+    def count_device_trackers(self, state):
+        count = 0
+        for sensor in self._device_trackers:
+            if self.get_state(sensor) == state:
+                count = count + 1
+        return count
+
+    def count_home_device_trackers(self):
+        return self.count_device_trackers("home")
+
+    def count_not_home_device_trackers(self):
+        return self.count_device_trackers("not_home")
+
+    def in_guest_mode(self):
+        if self._guest_control is None:
+            return False
+        if self.get_state(self._guest_control) == 'on':
+            return True
+        else:
+            return False
+
+    def in_vacation_mode(self):
+        if self._vacation_control is None:
+            return False
+        if self.get_state(self._vacation_control) == 'on':
+            return True
+        else:
+            return False
+
+    def power_on_callback(self, entity, attribute, old, new, kwargs):
+        self.log(
+            "Callback power_on from {}:{} {}->{}".format(entity, attribute, old, new))
+
+        for device_control in self._power_controls:
+            self.turn_on(device_control)
+
+    def power_off_motion_callback(self, entity, attribute, old, new, kwargs):
+        self.log(
+            "Callback power_off_motion from {}:{} {}->{}".format(entity, attribute, old, new))
+
+        for device_control in self._power_controls:
+            self.turn_off(device_control)
+
+    def power_off_presence_callback(self, entity, attribute, old, new, kwargs):
+        self.log(
+            "Callback power_off_presence from {}:{} {}->{}".format(entity, attribute, old, new))
+
+        if(self.count_home_device_trackers() > 0):
+            self.log("Ignoring status {} of {} because {} device_trackers are still at home".format(
+                new, entity, self.count_home_device_trackers()))
+            return
+
+        if(self.in_guest_mode()):
+            self.log("Ignoring status {} of {} because {} we have guests".format(
+                new, entity, self.count_home_device_trackers()))
+            return
+
+        for device_control in self._power_controls:
+            self.turn_off(device_control)
